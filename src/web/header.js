cd379b6c16f1d1e54e390812e9db43eb10596914
Moka Sasaki <moka.sasaki@rakus-partners.co.jp>
2024-02-21 16:22:22 +0900

    Add header.js

---
+++ src/web/header.js
@@ -0,0 +1,31 @@
+// header.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = `
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #ffd700;">
+        <a class="navbar-brand" href="#">顧客管理システム</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto mr-2">
+                <li class="nav-item">
+                    <a class="nav-link" href="http://dev.marathon.rplearn.net/moka_sasaki/customer/add.html">追加</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="http://dev.marathon.rplearn.net/moka_sasaki/customer/list.html">一覧</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <style>
+        /* ナビゲーションバーのボタンをマウスオーバーしたときのスタイル */
+        .navbar-nav .nav-link:hover {
+            background-color: orange !important;
+            color: white !important;
+        }
+    </style>
+    `;
+    document.body.insertAdjacentHTML('afterbegin', navbar);
+});
